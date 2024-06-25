8cf8f44124d840578368bce134eed8749cfca007
arunegida <arunegida@gamil.com>
2024-06-25 09:10:02 +0530

    feat: make github sync

--- locals.tf
+++ locals.tf
@@ -13,7 +13,7 @@ workspace = {
       vcs_repo_identifier="${var.github_organization_name}/fem-eci-terraform-tfe"
 }
   "devops-learning-github"={
-      execution_mode="local"
+      execution_mode="remote"
       description = "Learning Workspace Terraform"
       project_id=module.project["devops-learning-project"].id
       vcs_repo_identifier="${var.github_organization_name}/fem-eci-terraform-github"
